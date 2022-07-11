3ca7dc208cd06eae4b644eca97233360eb78edd0
Stino01 <agostino.carrieri01@gmail.com>
2022-07-11 17:27:56 +0200

    connect user to product

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -8,8 +8,7 @@
             <img src="https://media-assets.lacucinaitaliana.it/photos/61fb0393f9bff304ce3ec288/16:9/w_2560%2Cc_limit/Il-meglio-del-lago-di-Orta.jpg" alt="user" style="width: 300px">
             <h1 class="text-center">{{$user->name}}</h1>
             <p class="text-center">{{$user->address}}</p>
-            {{-- {{route('admin.products.index')}} --}}
-            <a href="" class="btn btn-primary text-uppercase my-5 col-2" type="button">Prodotti</a>
+            <a href="{{route('admin.products.index')}}" class="btn btn-primary text-uppercase my-5 col-2" type="button">Prodotti</a>
             <form action="{{route('admin.users.destroy', $user->id)}}" method="post">
                 @csrf
                 @method('DELETE')
