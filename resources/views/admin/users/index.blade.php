43509c2a9dba32e4a7c9e4686efbb4b5674d00f1
Stino01 <agostino.carrieri01@gmail.com>
2022-07-11 17:00:02 +0200

    add crud delete usercontroller

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -2,13 +2,18 @@
 
 @section('content')
     <h1 class="text-center">Index</h1>
-    <div class="container">
+    <div class="container d-flex">
         @foreach ($users as $user)
-        <a href="{{route('admin.users.show', $user->id)}}"><div class="card col-4 d-flex justify-content-center align-items-center p-5">
+        <div class="card col-4 d-flex justify-content-center align-items-center p-5">
             <img src="https://media-assets.lacucinaitaliana.it/photos/61fb0393f9bff304ce3ec288/16:9/w_2560%2Cc_limit/Il-meglio-del-lago-di-Orta.jpg" alt="user" style="width: 300px">
-            <h2>{{$user->name}}</h2>
+            <h2><a href="{{route('admin.users.show', $user->id)}}">{{$user->name}}</a></h2>
             <p>{{$user->address}}</p>
-        </div></a>
+            <form action="{{route('admin.users.destroy', $user->id)}}" method="post">
+                @csrf
+                @method('DELETE')
+                <input type="submit" class="btn btn-danger text-uppercase col-12" value="Delete">
+            </form>
+        </div>
         @endforeach
     </div>
 @endsection
--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
     <h1 class="text-center">Show</h1>
-    <div class="container">
+    <div class="container d-flex align-items-center flex-column">
         <h1 class="text-center">{{$user->name}}</h1>
         <p class="text-center">{{$user->address}}</p>
-        <a href="{{route('admin.users.edit', $user->id)}}" class="btn btn-primary text-uppercase my-5" type="button">Edit</a>
+        <a href="{{route('admin.users.edit', $user->id)}}" class="btn btn-primary text-uppercase my-5 col-2" type="button">Edit</a>
     </div>
 @endsection
