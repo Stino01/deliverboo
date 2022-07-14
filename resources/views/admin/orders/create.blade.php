a285a5432be5a51cafa8a800f51fd2a2cdf206cd
Penna92 <giacomopenna92@gmail.com>
2022-07-14 13:01:01 +0200

    Inizializzato order controller e blades

---
+++ resources/views/admin/orders/create.blade.php
@@ -0,0 +1,94 @@
+@extends('layouts.admin')
+@section('content')
+<div class="container">
+    <h2>Inserisci un nuovo prodotto</h2>
+    <p class="fst-italic">I campi contrassegnati con * sono obbligatori</p>
+    <form action="{{route('admin.products.store')}}" method="POST" enctype="multipart/form-data">
+        @csrf
+
+        {{-- NOME --}}
+        <div class="mb-3">
+            <label for="name" class="form-label">Nome prodotto *</label>
+            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name"
+                placeholder="Inserisci il titolo">
+            @error('name')
+            <div class="alert alert-danger">{{$message}}</div>
+            @enderror
+        </div>
+
+        {{-- DESCRIZIONE --}}
+        <div class="mb-3">
+            <label for="description" class="form-label @error('description') is-invalid @enderror">Descrizione
+                prodotto</label>
+            <textarea name="description" id="description" class="form-control" cols="30" rows="10"></textarea>
+            @error('description')
+            <div class="alert alert-danger">{{$message}}</div>
+            @enderror
+        </div>
+
+        {{-- CATEGORIA --}}
+        <div class="mb-3">
+            <label for="category" class="form-label">Categoria *</label>
+            <select class="form-control @error('category_id') is-invalid @enderror" id="category" name="category_id">
+                <option value="">Selezione la categoria</option>
+                @foreach ($categories as $category)
+                <option value="{{$category->id}}">{{$category->name}}</option>
+                @endforeach
+            </select>
+            @error('category_id')
+            <div class="alert alert-danger">{{$message}}</div>
+            @enderror
+        </div>
+
+        {{-- IMAGE --}}
+        <div class="form-group">
+            <img id="uploadPreview" width="100" src="https://via.placeholder.com/300x200">
+            <label for="image">Aggiungi immagine</label>
+            <input type="file" id="image" name="image" onchange="boolpress.previewImage();">
+            {{-- @error('image')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror --}}
+        </div>
+
+        {{-- PRICE --}}
+        <div class="form-group mb-3">
+            <label for="price" class="form-label @error('price') is-invalid @enderror">Prezzo *</label>
+            <input type="number" step="0.10" class="form-control" id="price" name="price" min='1' max='999.90'
+                aria-describedby="price" placeholder="Es: 2.90">
+            @error('price')
+            <div class="alert alert-danger">{{$message}}</div>
+            @enderror
+        </div>
+
+        {{-- TAGS --}}
+        {{-- <div class="mb-3">
+            <div class="form-group">
+                <h5>Tags</h5>
+                @foreach ($tags as $tag)
+                <div class="form-check-inline">
+                    <input type="checkbox" class="form-check-input" {{in_array($tag->id, old("tags", [])) ? 'checked' :
+                    '' }}
+                    id="{{$tag->slug}}" name="tags[]" value="{{$tag->id}}">
+                    <label class="form-check-label" for="{{$tag->slug}}">{{$tag->name}}</label>
+                </div>
+                @endforeach
+            </div>
+        </div> --}}
+
+        {{-- VISIBLE --}}
+        <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" {{old('visible') ? 'checked' : '' }} id="visible"
+                name="visible">
+            <label class="form-check-label" for="visible">Visibile</label>
+        </div>
+
+        {{-- BOTTONE --}}
+        <button type="submit" class="btn btn-primary">Crea</button>
+    </form>
+</div>
+<script src="//js.nicedit.com/nicEdit-latest.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+    bkLib.onDomLoaded(nicEditors.allTextAreas);
+</script>
+@endsection
---
+++ resources/views/admin/orders/show.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.admin')
+@include('partials/popupdelete')
+@section('content')
+<div class="container">
+    @if(session()->has('message'))
+    <div class="alert alert-success">
+        {{session()->get('message')}}
+    </div>
+    @endif
+    <div class="card d-flex flex-column my-3 gap-3">
+        <div class="card-header">
+            <div class="container text-center">
+                <h1>{{$product->name}}</h1>
+            </div>
+        </div>
+        <div class="card-body d-flex flex-column gap-3">
+            <div>
+                <small class="mr-3">In magazzino dal: {{$product->created_at}}</small>
+                {{-- condizione per stampare il valore corretto a seconda del booleano --}}
+                @if($product->visible !== 0)
+                <p class="badge-success d-inline-block p-1 rounded">Disponibile</p>
+                @else
+                <p class="badge-danger d-inline-block p-1 rounded">Non disponibile</p>
+                @endif
+            </div>
+            @if($product->description !== null)
+            <div>
+                <h2>Descrizione: </h2>
+                <p>{!! $product->description !!}</p>
+            </div>
+            @endif
+            @foreach($categories as $category)
+            @if($product->category_id == $category->id)
+            <h5>Categoria: {{$category->name}}</h5>
+            @endif
+            @endforeach
+            @if($product->category_id == null)
+            <h5>Nessuna categoria assegnata</h5>
+            @endif
+            @if($product->image != null)
+            <img src="{{ asset('storage/' . $product->image) }}" class="rounded " alt="{{ $product->title }}">
+            @endif
+            {{-- @if (count($product->tags)>0)
+            <h5>Tags</h5>
+            <ul>
+                @foreach($product->tags as $item)
+                <li>{{$item->name}}</li>
+                @endforeach
+            </ul>
+            @endif --}}
+            {{-- <div class="card">
+                <ul>
+                    <div class="card-header">
+                        <h4>Commenti:</h4>
+                    </div>
+                </ul>
+            </div> --}}
+            <div class="d-flex align-items-start">
+                <a href="{{ route('admin.products.edit', $product->id) }}" class="btn btn-warning mr-2">Edit</a>
+                <form action="{{ route('admin.products.destroy', $product->id) }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" onclick="boolpress.openModal(event, {{ $product->id }})"
+                        class="btn btn-danger delete">Delete</button>
+                </form>
+            </div>
+            {{-- @endif --}}
+        </div>
+    </div>
+    @endsection
