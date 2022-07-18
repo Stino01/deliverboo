000fca779cd4adfd5ef8b830ca34022cef52f93e
Penna92 <giacomopenna92@gmail.com>
2022-07-18 20:05:06 +0200

    Risolvere routing che non va

---
+++ resources/js/components/InputComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <select
+      class="form-select mt-3"
+      name="type"
+      id="type"
+      v-model="selectValue"
+      @change="searchType"
+    >
+      <option value="">Tipologia</option>
+      <option
+        :value="index"
+        v-for="(type, index) in restaurantTypes"
+        :key="index"
+      >
+        {{ type.id }} - {{ type.name }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputComponent",
+  data() {
+    return {
+      selectValue: "",
+    };
+  },
+  props: ["restaurantTypes"],
+  methods: {
+    searchType() {
+      this.$emit("searchType", this.selectValue);
+      //   this.selectValue = "";
+    },
+    // reset() {
+    //   this.selectValue = "";
+    //   this.$emit("search", this.selectValue);
+    // },
+  },
+};
+</script>
+
+<style>
+</style>
--- resources/js/components/MainComponent.vue
+++ resources/js/components/MainComponent.vue
@@ -3,40 +3,85 @@
     <SloganComponent />
     <TypesSlider />
 
-    <label type="text" for="type">Type</label>
-    <select class="form-control" name="type" id="type" v-model="inputText" @change="changeSearch()">
-      <option value="">All</option>
-      <option :value="index" v-for="(type, index) in types" :key="index">{{ type.name }}</option>
+    <!-- <label type="text" for="type">Type</label>
+    <select
+      class="form-control"
+      name="type"
+      id="type"
+      v-model="inputText"
+      @change="changeSearch"
+    > -->
+    <!-- <option value="">All</option> -->
+    <!-- <option :value="index" v-for="(type, index) in types" :key="index">
+        {{ type.id }} - {{ type.name }}
+      </option>
     </select>
 
     <ul>
-      <li v-for="(restaurant, index) in restaurants" :key="index">
-        {{ restaurant.name }}
+      <li v-for="(restaurant, index) in filteredList" :key="index">
+        <a v-if="restaurant.pivot_type_id == searchText" href=""
+          >{{ restaurant.name }} - {{ restaurant.pivot_type_id }}</a
+        >
       </li>
-    </ul>
-
+    </ul> -->
+    <InputComponent
+      @searchType="setSearchType($event)"
+      :restaurantTypes="types"
+    />
+    <div
+      v-if="filteredList.length > 0"
+      class="row d-flex justify-content-around my-5"
+    >
+      <div
+        v-for="restaurant in filteredList"
+        :key="restaurant.index"
+        class="
+          d-flex
+          justify-content-center
+          col-6 col-sm-4 col-md-3 col-lg-2
+          m-3
+        "
+      >
+        <div class="card">
+          <div class="card-header">
+            {{ restaurant.name }}
+          </div>
+          <div class="card-body">
+            <router-link
+              class="mx-2 btn btn_main"
+              :to="{ name: 'single-restaurant', params: { slug: restaurant.slug } }"
+            >
+              Visualizza Ristorante
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
 import TypesSlider from "./TypesSlider.vue";
 import SloganComponent from "./SloganComponent.vue";
+import InputComponent from "./InputComponent.vue";
 export default {
   name: "MainComponent",
-  components: { TypesSlider, SloganComponent },
+  components: { TypesSlider, SloganComponent, InputComponent },
   data() {
     return {
       types: [],
       restaurants: [],
-      inputText: '',
+      inputText: "",
       searchText: "",
     };
   },
   methods: {
-    changeSearch(){
-        this.searchText = this.inputText
-        //console.log(this.searchText)
-    }
+    setSearchType(index) {
+      // this.searchText = this.inputText + 1;
+      this.searchText = index + 1;
+      console.log(this.searchText);
+      console.log(this.filteredList);
+    },
   },
   created() {
     //CHIAMATA AXIOS PER TYPES
@@ -60,9 +105,18 @@ export default {
         console.log(error);
       });
   },
+  computed: {
+    filteredList() {
+      if (this.searchText === "") {
+        return this.restaurants;
+      }
+      return this.restaurants.filter((el) => {
+        return el.pivot_type_id === this.searchText;
+      });
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-
 </style>
