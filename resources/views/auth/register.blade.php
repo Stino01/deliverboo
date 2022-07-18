62b4fdf12624af67206360a4cfd1ff14f24459b5
Fabrizio Zivolo <fabrizio.zivolo@gmail.com>
2022-07-18 09:52:59 +0200

    fixed color style register/login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,14 +5,14 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header text-white">{{ __('Register') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}*</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right text-white">{{ __('Nome') }}*</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -26,7 +26,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="surname" class="col-md-4 col-form-label text-md-right">{{ __('Cognome') }}*</label>
+                            <label for="surname" class="col-md-4 col-form-label text-md-right text-white">{{ __('Cognome') }}*</label>
 
                             <div class="col-md-6">
                                 <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autocomplete="surname" autofocus>
@@ -40,7 +40,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Indirizzo E-Mail') }}*</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right text-white">{{ __('Indirizzo E-Mail') }}*</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -54,7 +54,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}*</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right text-white">{{ __('Password') }}*</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -68,7 +68,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Conferma Password') }}*</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right text-white">{{ __('Conferma Password') }}*</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
@@ -80,7 +80,7 @@
                                 <button type="submit" class="btn btn-submit">
                                     {{ __('Registrati') }}
                                 </button>
-                                <span>(*) Campi obbligatori</span>
+                                <span class="text-white">(*) Campi obbligatori</span>
                             </div>
                         </div>
                     </form>
