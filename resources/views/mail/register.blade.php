ac7088d793d1d398ceeb2332bc76090e188f9660
Stino01 <agostino.carrieri01@gmail.com>
2022-07-27 16:51:33 +0200

    hsfd<fgsdhafd

---
+++ resources/views/mail/register.blade.php
@@ -0,0 +1,5 @@
+<h1>Grazie per esserti registrato!</h1>
+<h3>Inserisci le informazioni del tuo ristorante e inizia a vendere con noi!</h3>
+<hr>
+
+<a href="http://127.0.0.1:8000/">Continua ad acquistare</a>
