dde1c1f2e95520ed607409d3ccf3783270c8df6c
Penna92 <giacomopenna92@gmail.com>
2022-07-27 18:31:51 +0200

    fixate success e failed blade.

--- resources/views/orders/failed.blade.php
+++ resources/views/orders/failed.blade.php
@@ -1,21 +1,34 @@
 @extends('layouts.admin')
 @section('content')
 <div class="container d-flex justify-content-center align-items-center">
-    <div class="card">
-        <h2 class="text-center my-4">Transazione fallita!</h2>
-        <a href="/"><button class="btn btn-primary cent">Torna alla home</button></a>
+    <div class="card text-white">
+        <h2 class="text-center my-4"><i class="fa-solid fa-triangle-exclamation"> </i>Transazione fallita! <i
+                class="fa-solid fa-skull-crossbones"></i></h2>
+        <a href="/"><button class="btn btn_main cent mt-5">Torna alla home</button></a>
     </div>
 </div>
 @endsection
 
 <style>
-.card {
-    background-color: transparent !important;
-    border: none !important;
-}
-.cent {
-    position: relative;
-    left: 50%;
-    transform: translateX(-50%);
-}
+    .card {
+        padding: 3rem;
+        background-color: #042940 !important;
+        border: none !important;
+        border-radius: 1rem !important;
+    }
+
+    h2 {
+        color: red;
+    }
+
+    .cent {
+        position: relative;
+        left: 50%;
+        transform: translateX(-50%);
+    }
+
+    .fa-skull-crossbones,
+    .fa-triangle-exclamation {
+        color: red;
+    }
 </style>
--- resources/views/orders/success.blade.php
+++ resources/views/orders/success.blade.php
@@ -1,18 +1,22 @@
 @extends('layouts.admin')
 @section('content')
 <div class="container d-flex justify-content-center align-items-center">
-    <div class="card">
+    <div class="card" id="success-payment-card">
         <h2 class="text-center my-3">Il tuo ordine è stato processato con successo!</h2>
-        <h3 class="text-center my-3">Buon appetito!</h3>
-        <a href="/"><button class="btn btn-primary cent">Torna alla home</button></a>
+        <h3 class="text-center my-3"><i class="fa-solid fa-burger"></i> Buon appetito! <i
+                class="fa-solid fa-face-smile"></i></h3>
+        <a href="/"><button class="btn btn_main cent mt-5">Torna alla home</button></a>
     </div>
 </div>
 @endsection
 
 <style scoped>
-    .card {
-        background-color: red !important;
+    #success-payment-card {
+        padding: 3rem;
+        background-color: #042940 !important;
         border: none !important;
+        color: white;
+        border-radius: 1rem !important;
     }
 
     .cent {
