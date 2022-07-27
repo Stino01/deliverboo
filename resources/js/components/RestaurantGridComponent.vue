32f2e259c45e05e024f6a7c6b4bff086b73f6115
Penna92 <giacomopenna92@gmail.com>
2022-07-27 22:37:40 +0200

    tolte virgole finali dalle descrizioni

--- resources/js/components/RestaurantGridComponent.vue
+++ resources/js/components/RestaurantGridComponent.vue
@@ -1,139 +1,153 @@
 <template>
-    <section>
-        <!-- <TypesSlider /> -->
+  <section>
+    <!-- <TypesSlider /> -->
 
-        <div class="container my-5">
-            <div class="row">
-                <div
-                    v-for="(type, index) in types"
-                    :key="index"
-                    class="col-6 col-sm-6 col-md-3 col-lg-2 py-2 checkbox"
-                >
-                    <input
-                        type="checkbox"
-                        name="type"
-                        id="type"
-                        :style="
-                            'background-image: url(/media/img/type_' +
-                            type.id +
-                            '.png);' +
-                            'background-color:' +
-                            type.background_color +
-                            ';'
-                        "
-                        :value="index + 1"
-                        v-model="selectValue"
-                        @change="searchType()"
-                    />
-                    <label
-                        class="text-uppercase font-weight-bold pt-2 label_text"
-                        for="type"
-                        >{{ type.name }}</label
-                    >
-                </div>
-            </div>
-        </div>
-        <h2 v-if="selectValue.length == 0" class="text-center">
-            I più amati nella tua zona
-        </h2>
+    <div class="container my-5">
+      <div class="row">
         <div
-            v-if="restaurants.length > 0"
-            class="row d-flex justify-content-around my-5 card-resturant-container container centratura"
+          v-for="(type, index) in types"
+          :key="index"
+          class="col-6 col-sm-6 col-md-3 col-lg-2 py-2 checkbox"
         >
-            <div
-                v-for="restaurant in restaurants"
-                :key="restaurant.index"
-                class="d-flex justify-content-center col-lg-3 col-md-4 col-sm-6 mb-4 mb-md-0"
-            >
-                <div class="card my-4" style="width: 18rem">
-                    <router-link
-                        class=""
-                        :to="{
-                            name: 'single-restaurant',
-                            params: { slug: restaurant.slug },
-                        }"
-                    >
-                        <img
-                            class="card-img-top img-fluid"
-                            :src="'storage/' + restaurant.image"
-                            :alt="'immagine di' + restaurant.name"
-                        />
-
-                        <div class="card-body">
-                            <h5 class="card-title text-dark">
-                                {{ restaurant.name }}
-                            </h5>
-                            <p class="card-text text-dark p_fixed">
-                                {{ restaurant.address }}
-                            </p>
-                        </div>
-                    </router-link>
-                </div>
-            </div>
+          <input
+            type="checkbox"
+            name="type"
+            id="type"
+            :style="
+              'background-image: url(/media/img/type_' +
+              type.id +
+              '.png);' +
+              'background-color:' +
+              type.background_color +
+              ';'
+            "
+            :value="index + 1"
+            v-model="selectValue"
+            @change="searchType()"
+          />
+          <label
+            class="text-uppercase font-weight-bold pt-2 label_text"
+            for="type"
+            >{{ type.name }}</label
+          >
         </div>
-        <!-- <div v-if="finalRestaurants.length == 0" class="container">
+      </div>
+    </div>
+    <h2 v-if="selectValue.length == 0" class="text-center">
+      I più amati nella tua zona
+    </h2>
+    <div
+      v-if="restaurants.length > 0"
+      class="
+        row
+        d-flex
+        justify-content-around
+        my-5
+        card-resturant-container
+        container
+        centratura
+      "
+    >
+      <div
+        v-for="restaurant in restaurants"
+        :key="restaurant.index"
+        class="
+          d-flex
+          justify-content-center
+          col-lg-3 col-md-4 col-sm-6
+          mb-4 mb-md-0
+        "
+      >
+        <div class="card my-4" style="width: 18rem">
+          <router-link
+            class=""
+            :to="{
+              name: 'single-restaurant',
+              params: { slug: restaurant.slug },
+            }"
+          >
+            <img
+              class="card-img-top img-fluid"
+              :src="'storage/' + restaurant.image"
+              :alt="'immagine di' + restaurant.name"
+            />
+
+            <div class="card-body">
+              <h5 class="card-title text-dark">
+                {{ restaurant.name }}
+              </h5>
+              <p class="card-text text-dark p_fixed">
+                <i class="fa-solid fa-map-location-dot"></i> Springfield -
+                {{ restaurant.address }}
+              </p>
+            </div>
+          </router-link>
+        </div>
+      </div>
+    </div>
+    <!-- <div v-if="finalRestaurants.length == 0" class="container">
       <p>Non ci sono ristoranti che corrispondono a questa tipologia</p>
     </div> -->
-    </section>
+  </section>
 </template>
 
 <script>
 // import InputComponent from "./InputComponent.vue";
 import TypesSlider from "./TypesSlider.vue";
 export default {
-    name: "RestaurantGridComponent",
-    components: { TypesSlider },
-    data() {
-        return {
-            types: [],
-            restaurants: [],
-            selectValue: [],
-            apiPath: "api/restaurants",
-        };
+  name: "RestaurantGridComponent",
+  components: { TypesSlider },
+  data() {
+    return {
+      types: [],
+      restaurants: [],
+      selectValue: [],
+      apiPath: "api/restaurants",
+    };
+  },
+  methods: {
+    getApiTypes() {
+      axios
+        .get("api/types")
+        .then((response) => {
+          this.types = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
-    methods: {
-        getApiTypes() {
-            axios
-                .get("api/types")
-                .then((response) => {
-                    this.types = response.data;
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
-        },
-        searchType() {
-            if (this.selectValue.length > 0) {
-                let finalQuery = "";
-                this.selectValue.forEach((el) => {
-                    finalQuery += el + ",";
-                });
+    searchType() {
+      if (this.selectValue.length > 0) {
+        let finalQuery = "";
+        this.selectValue.forEach((el) => {
+          finalQuery += el + ",";
+        });
 
-                axios
-                    .get(`${this.apiPath}?type=${finalQuery}`)
-                    .then((response) => {
-                        this.restaurants = response.data;
-                    })
-                    .catch((error) => {
-                        console.log(error);
-                    });
-            } else {
-                axios
-                    .get(this.apiPath)
-                    .then((response) => {
-                        // console.log(response.data);
-                        this.restaurants = response.data;
-                    })
-                    .catch((error) => {
-                        console.log(error);
-                    });
-            }
-        },
+        axios
+          .get(`${this.apiPath}?type=${finalQuery}`)
+          .then((response) => {
+            this.restaurants = response.data;
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      } else {
+        axios
+          .get(this.apiPath)
+          .then((response) => {
+            // console.log(response.data);
+            this.restaurants = response.data;
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
     },
-    created() {
-        this.getApiTypes();
-        this.searchType();
-    },
+  },
+  created() {
+    this.getApiTypes();
+    this.searchType();
+  },
 };
 </script>
 
@@ -181,73 +195,72 @@ export default {
 //     }
 // }
 .centratura {
-    margin: 0 auto;
+  margin: 0 auto;
 }
 
 .card {
-    padding: 0 !important;
-    text-align: center;
-    color: black;
-    border-radius: 10px;
-    box-shadow: 0px 4px 13px 0px rgba(0, 0, 0, 0.52);
-    transition: transform 0.3s ease-in-out;
-    img {
-        width: 100% !important;
-        height: 200px !important;
-        object-fit: cover;
-        border-radius: 10px 10px 0 0;
-    }
-    &:hover {
-        transform: scale(1.05);
-        box-shadow: 0px 8px 13px 0px rgba(0, 0, 0, 0.7);
-        transition: 0.5s;
-    }
+  padding: 0 !important;
+  text-align: center;
+  color: black;
+  border-radius: 10px;
+  box-shadow: 0px 4px 13px 0px rgba(0, 0, 0, 0.52);
+  transition: transform 0.3s ease-in-out;
+  img {
+    width: 100% !important;
+    height: 200px !important;
+    object-fit: cover;
+    border-radius: 10px 10px 0 0;
+  }
+  &:hover {
+    transform: scale(1.05);
+    box-shadow: 0px 8px 13px 0px rgba(0, 0, 0, 0.7);
+    transition: 0.5s;
+  }
 }
 
 :hover a {
-    text-decoration: none;
+  text-decoration: none;
 }
 .checkbox {
-    display: inline-flex;
+  display: inline-flex;
+  cursor: pointer;
+  position: relative;
+  flex-direction: column;
+  align-items: center;
+  transition: transform 0.3s ease-in-out;
+
+  & > input {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    -o-appearance: none;
+    appearance: none;
+    outline: none;
+    height: 8rem;
+    width: 8rem;
+    // border: 1px solid #34495e;
+    border-radius: 50%;
+    transition-duration: 0.3s;
+    background-size: 80%;
+    background-repeat: no-repeat;
+    background-position: center;
     cursor: pointer;
-    position: relative;
-    flex-direction: column;
-    align-items: center;
-    transition: transform 0.3s ease-in-out;
+  }
 
+  & > input:checked {
+    border: 0.5rem solid #dbf227;
+    transform: scale(1.05);
+    transition: 0.3s;
+    box-shadow: 0px 8px 13px 0px rgba(0, 0, 0, 0.3);
+  }
 
-    & > input {
-        -webkit-appearance: none;
-        -moz-appearance: none;
-        -o-appearance: none;
-        appearance: none;
-        outline: none;
-        height: 8rem;
-        width: 8rem;
-        // border: 1px solid #34495e;
-        border-radius: 50%;
-        transition-duration: 0.3s;
-        background-size: 80%;
-        background-repeat: no-repeat;
-        background-position: center;
-        cursor: pointer;
-    }
+  & > input:hover {
+    transform: scale(1.05);
+    transition: 0.3s;
+    box-shadow: 0px 8px 13px 0px rgba(0, 0, 0, 0.3);
+  }
 
-    & > input:checked {
-        border: 0.5rem solid #dbf227;
-        transform: scale(1.05);
-        transition: 0.3s;
-        box-shadow: 0px 8px 13px 0px rgba(0, 0, 0, 0.3);
-    }
-
-    & > input:hover {
-        transform: scale(1.05);
-        transition: 0.3s;
-        box-shadow: 0px 8px 13px 0px rgba(0, 0, 0, 0.3);
-    }
-
-    & > input:active {
-        border: 0.5rem solid #005c53;
-    }
+  & > input:active {
+    border: 0.5rem solid #005c53;
+  }
 }
 </style>
